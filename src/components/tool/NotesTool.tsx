4d3677682e94fd8e65198eb683771a52f0aef19f
SuhasKanwar <suhas.kanwar@gmail.com>
2025-11-08 21:31:17 +0530

    Building the UI of the notes tool page

--- src/components/tool/NotesTool.tsx
+++ src/components/tool/NotesTool.tsx
@@ -1,7 +1,22 @@
+"use client";
+
+import { useState } from "react";
+import { FileUpload } from "../ui/file-upload";
+
 export default function NotesTool() {
-    return (
-        <section className="flex flex-col w-full px-3 pt-4 items-center h-full mt-[var(--navbar-height,64px)]">
-            Notes Tool
-        </section>
-    );
+	const [files, setFiles] = useState<File[]>([]);
+
+	const handleFileUpload = (files: File[]) => {
+		setFiles(files);
+		console.log("Uploaded files:", files);
+	}
+
+	return (
+		<section className="flex bg-white flex-col w-full px-3 pt-4 items-center h-full mt-[var(--navbar-height,64px)]">
+			<h2>
+				Notes Tool (OCR)
+			</h2>
+			<FileUpload onChange={handleFileUpload} />
+		</section>
+	);
 }
