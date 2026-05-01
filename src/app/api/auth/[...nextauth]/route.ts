7681188f8f02692ffe15c14dc48ae497b064e9f1
Suhas Kanwar <suhas.kanwar@gmail.com>
2026-05-01 22:25:03 +0530

    fixed the gauth prod prob

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,17 @@
 import NextAuth from "next-auth";
 import { authOptions } from "./options";
+import { NextRequest } from "next/server";
 
-const handler = NextAuth(authOptions);
+const handler = async (req: NextRequest, ctx: any) => {
+    if (process.env.NODE_ENV === "production" && !process.env.NEXTAUTH_URL) {
+        const proto = req.headers.get("x-forwarded-proto") || "https";
+        const host = req.headers.get("x-forwarded-host") || req.headers.get("host");
+        if (host) {
+            process.env.NEXTAUTH_URL = `${proto}://${host}`;
+        }
+    }
+
+    return NextAuth(req, ctx, authOptions);
+};
 
 export { handler as GET, handler as POST };
